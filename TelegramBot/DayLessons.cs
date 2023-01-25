da684d685a1fa73c6292b6635ac49b06bde68a14
Anton <antontokmakov757@gmail.com>
2023-01-26 00:42:05 +0700

    telegramBot, no checks input data

---
+++ TelegramBot/DayLessons.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace TelegramBot
+{
+    public class DayLessons
+    {
+        public string[] lessons = new string[5];
+        public string[] teacher = new string[5];
+        public string[] offise = new string[5];
+
+
+        public int getCount()
+        {
+            return lessons.Length;
+        }
+
+        public void requestDB(string group/*, string even, string? weekday*/)
+        {
+            DB db = new DB();
+            DataTable dTable = new DataTable();
+            DateTime date = DateTime.Now;
+            var dayofweek = date.DayOfWeek;
+  
+            db.openConnection();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand($"SELECT * FROM datalesson WHERE group1 = '{group}' AND UpDown = 'Четное' AND weekday = '{dayofweek.ToString()}'", db.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(dTable);
+
+            db.closeConnection();
+
+            int count = dTable.Rows.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var dtRow = dTable.Rows[i];
+                lessons[i] = dtRow[2].ToString();
+                teacher[i] = dtRow[5].ToString();
+                offise[i] = dtRow[6].ToString();
+            }
+        }
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        async private static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+               string textMessage = "";
+               DayLessons dayLesson = new DayLessons();
+               dayLesson.requestDB(message.Text);                    
+                for (int i = 0; i < dayLesson.getCount(); i++)
+               {
+                    textMessage += String.Format("{0,10}|{1,10}|{2,10}\n", dayLesson.lessons[i], dayLesson.offise[i], dayLesson.teacher[i]);
+               }  
+                await botClient.SendTextMessageAsync(message.Chat.Id, textMessage);
+                return;
+            }
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
